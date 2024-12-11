0a2516a2675389d56668ad3bed1eca76051d6f0b
unknown <msotkasi20@students.oamk.fi>
2024-12-11 19:34:57 +0200

    user crud operaatiot tehty

---
+++ database.js
@@ -0,0 +1,8 @@
+const mysql = require('mysql2');
+const dotenv=require('dotenv');
+dotenv.config();
+
+//const myConnectionString = "mysql://netuser:netpass@localhost:3306/netdb";
+const myConnectionString = process.env.MYSQL_SERVER;
+const connection = mysql.createPool(myConnectionString);
+module.exports = connection;
